6cfa6995c1f5724018ba53e15e7b260fbdee5139
nguyenanh2222 <nguyenvieanh2103@gmail.com>
2022-07-22 17:33:24 +0700

    rs = session.execute(stmt).fetchall()

--- app/v2/router/admin/product.py
+++ app/v2/router/admin/product.py
@@ -1,5 +1,9 @@
+import os
 from decimal import Decimal
-from fastapi import APIRouter, Query, Depends
+from typing import List, Union
+
+from fastapi import APIRouter, Query, Depends, File, UploadFile
+from sqlalchemy.engine import Row
 from starlette import status
 from app.v2.router.admin.permission import get_user
 from app.v2.service.admin.product import ProductService
@@ -7,6 +11,7 @@ from project.core.schemas import DataResponse, PageResponse
 from project.core.schemas import Sort
 from project.core.swagger import swagger_response
 from schemas.product import ProductRes, ProductReq
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
@@ -30,7 +35,7 @@ def get_product(
         to_price: Decimal = Query(None, description="To price"),
         sort_direction: Sort.Direction = Query(None, description="Filter by"),
         service: ProductService = Depends(get_user)
-) -> PageResponse:
+):
     products = ProductService().get_product_service(
         page=page,
         size=size,
@@ -38,69 +43,92 @@ def get_product(
         category=category,
         product_name=product_name,
         from_price=from_price,
-
         to_price=to_price,
         sort_direction=sort_direction)
 
-    return PageResponse(data=products.data,
-                        total_page=products.total_page,
-                        total_items=products.total_items,
-                        current_page=products.current_page)
+    return products
 
 
-@router.get(
-    path="/{product_id}",
-    status_code=status.HTTP_200_OK,
-    responses=swagger_response(
-        response_model=DataResponse[ProductRes],
-        success_status_code=status.HTTP_200_OK,
-        fail_status_code=status.HTTP_404_NOT_FOUND
-    )
-)
-def get_product_by_id(product_id: int,
-                      service: ProductService = Depends(get_user)) -> DataResponse:
-    product = ProductService().get_product_by_id_service(
-        product_id=product_id)
-    return DataResponse(data=product)
-
-
-@router.put(
-    path="/{product_id}",
-    status_code=status.HTTP_200_OK,
-    responses=swagger_response(
-        response_model=DataResponse[ProductRes],
-        success_status_code=status.HTTP_200_OK,
-        fail_status_code=status.HTTP_400_BAD_REQUEST
-    )
-)
-def put_product_by_id(product: ProductReq, product_id: int,
-                      service: ProductService = Depends(get_user)) -> DataResponse:
-    product = ProductService().put_product_service(
-        product=product, product_id=product_id)
-    return DataResponse(data=product)
+# @router.get(
+#     path="/{product_id}",
+#     status_code=status.HTTP_200_OK,
+#     responses=swagger_response(
+#         response_model=DataResponse[ProductRes],
+#         success_status_code=status.HTTP_200_OK,
+#         fail_status_code=status.HTTP_404_NOT_FOUND
+#     )
+# )
+# def get_product_by_id(product_id: int,
+#                       service: ProductService = Depends(get_user)) -> DataResponse:
+#     product = ProductService().get_product_by_id_service(
+#         product_id=product_id)
+#     return DataResponse(data=product)
+#
+#
+# @router.put(
+#     path="/{product_id}",
+#     status_code=status.HTTP_200_OK,
+#     responses=swagger_response(
+#         response_model=DataResponse[ProductRes],
+#         success_status_code=status.HTTP_200_OK,
+#         fail_status_code=status.HTTP_400_BAD_REQUEST
+#     )
+# )
+# def put_product_by_id(product: ProductReq, product_id: int,
+#                       service: ProductService = Depends(get_user)) -> DataResponse:
+#     product = ProductService().put_product_service(
+#         product=product, product_id=product_id)
+#     return DataResponse(data=product)
+#
+# @router.post(
+#     path="/",
+#     status_code=status.HTTP_201_CREATED,
+#
+#     responses=swagger_response(
+#         response_model=DataResponse[ProductRes],
+#         success_status_code=status.HTTP_201_CREATED,
+#         fail_status_code=status.HTTP_400_BAD_REQUEST
+#     )
+# )
+# def post_product(product: ProductReq,
+#                  service: ProductService = Depends(get_user)) -> DataResponse:
+#     product = ProductService().post_product_service(
+#         product=product)
+#     return DataResponse(data=product)
+#
+#
+# @router.delete(
+#     path="/{product_id}",
+#     status_code=status.HTTP_204_NO_CONTENT
+# )
+# def delete_product(product_id: int,
+#                    service: ProductService = Depends(get_user)) -> DataResponse:
+#     product = ProductService().delete_product_service(product_id=product_id)
+#     return DataResponse(data=product)
 
 @router.post(
-    path="/",
-    status_code=status.HTTP_201_CREATED,
-
-    responses=swagger_response(
-        response_model=DataResponse[ProductRes],
-        success_status_code=status.HTTP_201_CREATED,
-        fail_status_code=status.HTTP_400_BAD_REQUEST
-    )
+    path="/files/"
 )
-def post_product(product: ProductReq,
-                 service: ProductService = Depends(get_user)) -> DataResponse:
-    product = ProductService().post_product_service(
-        product=product)
-    return DataResponse(data=product)
+def create_file(file: bytes = File(default=None)):
+    file = ProductService().create_file_service(file=file)
+    return {'file_size': len(file)}
 
-
-@router.delete(
-    path="/{product_id}",
-    status_code=status.HTTP_204_NO_CONTENT
+@router.post(
+    path="/uploadfiles/"
 )
-def delete_product(product_id: int,
-                   service: ProductService = Depends(get_user)) -> DataResponse:
-    product = ProductService().delete_product_service(product_id=product_id)
-    return DataResponse(data=product)
+async def create_upload_file(file: UploadFile):
+    if not file:
+        return {"message": "No upload file sent"}
+    file = await ProductService().create_upload_file_sevice(file=file)
+    return {'filename': file['filename']}
+
+@router.get(
+    path="/getfile",
+    response_class=FileResponse
+)
+def get_file(file_name: str):
+    if not file_name:
+        return {"message": "Name's file error"}
+    print(os.path.exists(f"fileupload/{file_name}"))
+    return FileResponse(f"fileupload/{file_name}")
+
--- app/v2/service/admin/product.py
+++ app/v2/service/admin/product.py
@@ -1,7 +1,11 @@
 import math
 from decimal import Decimal
+from typing import Dict
+
+from fastapi import File, UploadFile
 from fastapi import HTTPException
 from starlette import status
+
 from app.v2.repos.product import ProductRepository
 from project.core.schemas import Sort, PageResponse, DataResponse
 from schemas.product import ProductReq
@@ -31,26 +35,44 @@ class ProductService(ProductRepository):
         if page and size is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
-        return PageResponse(data=products,
-                            total_page=total_page,
-                            total_items=total_items,
-                            current_page=current_page)
+        return products
+        # total_page=total_page,
+        # total_items=total_items,
+        # current_page=current_page)
 
-    def get_product_by_id_service(self, product_id: int) -> DataResponse:
-        product = ProductRepository().get_product_by_id_repos(
-            product_id=product_id)
-        return DataResponse(data=product)
+    # def get_product_by_id_service(self, product_id: int) -> DataResponse:
+    #     product = ProductRepository().get_product_by_id_repos(
+    #         product_id=product_id)
+    #     return DataResponse(data=product)
+    #
+    # def put_product_service(self, product: ProductReq, product_id: int) -> DataResponse:
+    #     product = ProductRepository().put_product_repos(
+    #         product=product, product_id=product_id)
+    #     return DataResponse(data=product)
+    #
+    # def post_product_service(self, product: ProductReq) -> DataResponse:
+    #     product = ProductRepository().post_product_repos(
+    #         product=product)
+    #     return DataResponse(data=product)
+    #
+    # def delete_product_service(self, product_id: int) -> DataResponse:
+    #     product = ProductRepository().delete_product_repos(product_id=product_id)
+    #     return DataResponse(data=product)
 
-    def put_product_service(self, product: ProductReq, product_id: int) -> DataResponse:
-        product = ProductRepository().put_product_repos(
-            product=product, product_id=product_id)
-        return DataResponse(data=product)
+    def create_file_service(self, file: bytes = File(default=None)):
+        return {'file_size': len(file)}
 
-    def post_product_service(self, product: ProductReq) -> DataResponse:
-        product = ProductRepository().post_product_repos(
-            product=product)
-        return DataResponse(data=product)
+    async def create_upload_file_sevice(self, file: UploadFile):
+        data = await file.read()
+        file = {'filename': file.filename}
+        f = open(f"fileupload/{file['filename']}", "wb")
+        f.write(data)
+        f.close()
 
-    def delete_product_service(self, product_id: int) -> DataResponse:
-        product = ProductRepository().delete_product_repos(product_id=product_id)
-        return DataResponse(data=product)
+        f = open(f"fileupload/{file['filename']}", "rb")
+        print(f.read())
+        f.close()
+        return file
+
+    async def get_file(self, file_name: str):
+        file = {'filename': file_name}
